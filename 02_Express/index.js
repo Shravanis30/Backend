171919986857ff10c7244a6684f795c573b8b1e8
Shravanis30 <salunkeshravani561@gmail.com>
2024-09-16 00:20:02 +0530

    code

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -1,8 +1,9 @@
 import 'dotenv/config'
 import express, { json } from "express"
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
-
 const port = process.env.PORT || 5000
 
 // app.get("/", (req, res)=> {
@@ -15,13 +16,31 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 
- 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
 const coffeeData = []
 let nextId = 1
 
 // add a new coffee
 app.post("/coffees", (req, res) => {
-    const {coffee_name, price} = req.body
+  logger.info("Its a post request nade ti add a new coffee")
+  const {coffee_name, price} = req.body
     const newCoffee = {id: nextId++, coffee_name, price}
     coffeeData.push(newCoffee)
     res.status(201).send(newCoffee)
@@ -51,7 +70,7 @@ app.put('/coffees/:id', (req, res) => {
     const {coffee_name, price} = req.body
     coffee.coffee_name = coffee_name
     coffee.price = price
-    res.send(200).send(coffee)
+    res.status(200).send(coffee)
 })
 
 
