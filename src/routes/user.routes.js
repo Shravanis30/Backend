00b9a62fc94819c1f68b8f04fe780c2803a49d53
Shravanis30 <salunkeshravani561@gmail.com>
2025-06-10 01:20:25 +0530

    add user-related routes for profile, avatar, cover image, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(
-    upload.fields([ 
+    upload.fields([
         {
             name: "avatar",
             maxCount: 1
-        }, 
+        },
         {
             name: "coverImage",
             maxCount: 1
@@ -26,5 +26,16 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-account").post(verifyJWT, updateAccountDetails)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+// while getting the file data
+router.route("/avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").posy(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// while getting from url (params)
+router.route("/c/:username".get(verifyJWT, getUserChannelProfile))
 
 export default router
